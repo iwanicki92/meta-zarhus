964c860e30d8094eb032ee7552551e0346e82042
Pawel Langowski <pawel.langowski@3mdeb.com>
2024-11-18 08:50:11 +0100

    generate-changelog.sh: Add optional GH access token parameter
    Signed-off-by: Pawel Langowski <pawel.langowski@3mdeb.com>

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-RELEASE=$(grep '^DISTRO_VERSION ' ./meta-zarhus-distro/conf/distro/include/zarhus-distro-common.conf | cut -d'=' -f2 | tr -d ' "')
-echo $RELEASE
-
-docker run -t -v "$(pwd)":/app/ "orhunp/git-cliff:${TAG:-latest}" \
-  --prepend CHANGELOG.md --latest
+if [ $# -eq 1 ]; then
+  docker run -t -v "$(pwd)":/app/ "orhunp/git-cliff:${TAG:-latest}" \
+    --prepend CHANGELOG.md --latest --github-token "$1"
+else
+  docker run -t -v "$(pwd)":/app/ "orhunp/git-cliff:${TAG:-latest}" \
+    --prepend CHANGELOG.md --latest
+fi
